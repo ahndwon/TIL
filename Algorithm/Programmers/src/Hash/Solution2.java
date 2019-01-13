3232a768dc01bab9e63985c1061a0620a96da982
ahndwon <gahlok24@gmail.com>
2019-01-13 19:33:03 +0900

    Level2

--- Algorithm/Programmers/src/Hash/Solution2.java
+++ Algorithm/Programmers/src/Hash/Solution2.java
@@ -1,32 +1,34 @@
 package Hash;
 
+import java.util.HashMap;
+
 public class Solution2 {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        HashMap<String, Integer> substrings = new HashMap<>();
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            String prefix = phone_book[i];
 
-            for (int j = 0; j < phone_book.length; j++) {
-                if (j != i) {
-                    String target = phone_book[j];
-                    if (!check(prefix, target)) {
-                        answer = false;
-                    }
+        for (String phoneNumber : phone_book) {
+            for (int i = 0; i <= phoneNumber.length(); i++) {
+                String sub = phoneNumber.substring(0, i);
+                System.out.println("sub: " + sub);
+                if (!substrings.containsKey(sub)) {
+                    substrings.put(sub, 0);
+                } else {
+                    substrings.replace(sub, substrings.get(sub) + 1);
                 }
             }
         }
 
+        System.out.println(substrings);
+
+        for (String phoneNum: phone_book) {
+            if (substrings.containsKey(phoneNum) && substrings.get(phoneNum) > 0) {
+                answer = false;
+            }
+        }
 
         return answer;
     }
 
-    private boolean check(String prefix, String target) {
-        int length = prefix.length();
-
-        if (length > target.length())
-            return true;
-
-        return !prefix.equals(target.substring(0, length));
-    }
 }
