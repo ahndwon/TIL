b0ac5f7b5b3eaf34eb769edb8092bba082a04b1f
ahndwon <gahlok24@gmail.com>
2019-01-14 19:38:22 +0900

    Solution3

---
+++ Algorithm/Programmers/src/Hash/Main3.java
@@ -0,0 +1,10 @@
+package Hash;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Solution3 sol3 = new Solution3();
+
+        String[][] test = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(sol3.solution(test));
+    }
+}
