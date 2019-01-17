0d11b78b3350348b8820189127a438a905d6c39a
ahndwon <gahlok24@gmail.com>
2019-01-17 13:24:48 +0900

    Solution4

--- Algorithm/Programmers/src/Hash/Main4.java
+++ Algorithm/Programmers/src/Hash/Main4.java
@@ -1,4 +1,13 @@
 package Hash;
 
 public class Main4 {
+    public static void main(String[] args) {
+        Solution4 sol4 = new Solution4();
+
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+
+        int[] answers = sol4.solution(genres, plays);
+        System.out.println("answers: " + answers);
+    }
 }
