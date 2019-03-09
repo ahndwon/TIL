33515d7b24ddf9e9c54dbc7cf12c66b9f5451261
ahndwon <gahlok24@gmail.com>
2019-03-09 15:50:23 +0900

    bfs

---
+++ Algorithm/Programmers/src/Algorithm/BFS/PracticeGraph.java
@@ -0,0 +1,55 @@
+package Algorithm.BFS;
+
+import java.util.LinkedList;
+
+public class PracticeGraph {
+    private int V; // 노드의 개수
+    private LinkedList<Integer>[] adj;
+
+    PracticeGraph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < adj.length; i++)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean[] visited = new boolean[V];
+
+        LinkedList<Integer> queue = new LinkedList();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            for (int n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+}
+
+class PracticeMain {
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.BFS(2); /* 주어진 노드를 시작 노드로 BFS 탐색 */
+    }
+}
